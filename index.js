ee6da19741ab3f448ba6e4c9513715eab81e89e6
Anishkumar2000 <anishkumar8868@gmail.com>
2024-04-25 15:04:53 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+function on() {
+   document.getElementById("h").classList.add("a");
+   
+}
+function off() {
+   document.getElementById("h").classList.remove("a");
+   document.getElementById("h").classList.remove("b");
+   
+}
+function change() {
+   document.getElementById("h").classList.add("b")
+   
+}
